a738d82790171d801ee92341da28282c9da8cd6b
Руслан Каписъски <ruslank1@abv.bg>
2025-11-21 15:29:49 +0200

    Setup express
    Also render test data in order to ensure backend to frontend connection

--- api/src/server.js
+++ api/src/server.js
@@ -1,1 +1,14 @@
+import express, { Router } from "express";
 import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.get("/hotels", (req, res) => {
+  res.json({ hotels: ["hotel 1", "hotel 2", "hotel 3"] });
+});
+
+app.listen(5000, () =>
+  console.log("Server is running on http://localhost:5000")
+);
